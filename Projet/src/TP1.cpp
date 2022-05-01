b9261cdf4c963a230769c7552ec9b0a838c5e217
Margaux-code <margaux.langautier@gmail.com>
2022-05-01 14:15:22 +0200

    TP1 et TP2
    Le mixte du TP1 avec le FFT du TP2.

--- Projet/src/TP1.cpp
+++ Projet/src/TP1.cpp
@@ -1,314 +1,305 @@
 
 #include <Arduino.h>
 #include <avr/io.h>
-byte led =5;
+#include <Wire.h>
+#include <arduinoFFT.h>
+#include <Adafruit_SSD1306.h>
+
+byte led = 5;
 enum ADC_modes
 {
-  ADC_A0,
-ADC_A1,
-ADC_A2,
-ADC_A3,
-ADC_A4,
-ADC_A5,
-ADC_A6,
-ADC_A7,
-ADC_SENSOR,
-ADC_1V1,
-ADC_GND,
-ADC_AREF,
-ADC_VCC,
-FALLING_TRIGGER,
-RISING_TRIGGER,
-CHANGE_TRIGGER,
-ADC_AIN1,
-ANALOG_MUX,
-ADC_AIN0,
+    ADC_A0,
+    ADC_A1,
+    ADC_A2,
+    ADC_A3,
+    ADC_A4,
+    ADC_A5,
+    ADC_A6,
+    ADC_A7,
+    ADC_SENSOR,
+    ADC_1V1,
+    ADC_GND,
+    ADC_AREF,
+    ADC_VCC,
+    FALLING_TRIGGER,
+    RISING_TRIGGER,
+    CHANGE_TRIGGER,
+    ADC_AIN1,
+    ANALOG_MUX,
+    ADC_AIN0,
 };
 
+Adafruit_SSD1306 display(-1);
+#define SAMPLES 64
+double vReal[SAMPLES];
+double vImag[SAMPLES];
+byte peak;
+long maxpeak;
+char buf[5];
+
+arduinoFFT FFT = arduinoFFT();
 double temp;
 
-void ADC_enable () // activer l’ADC
+void ADC_enable() // activer l’ADC
 {
-cli ();
-ADCSRA |= (1 << ADEN);
-sei();
+    cli();
+    ADCSRA |= (1 << ADEN);
+    sei();
 }
 
-void ADC_disable ()
+void ADC_disable()
 {
-cli ();
-ADCSRA |= (1 << ADIE);
-sei();
+    cli();
+    ADCSRA |= (1 << ADIE);
+    sei();
 }
-void setAnalogMux(ADC_modes mux){
-cli();
-switch(mux){
-  case ADC_A0 :
-  ADMUX &=  ~((1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (1<<MUX0));
-  
-  break;
-  case ADC_A1 :
-  ADMUX &=  ~((1<<MUX3) | (1<<MUX2) | (1<<MUX1));
-  ADMUX |= (1<<MUX0);
-  break;
-  case ADC_A2 :
-  ADMUX &=  ~((1<<MUX3) | (1<<MUX2) | (1<<MUX0));
-  ADMUX |= (1<<MUX1);
-  break;
-  case ADC_A3 :
-  ADMUX &=  ~((1<<MUX3) | (1<<MUX2));
-  ADMUX |= ((1<<MUX1) | (1<<MUX0));
-  break;
-  case ADC_A4 :
-  ADMUX &=  ~((1<<MUX3)| (1<<MUX1) | (1<<MUX0));
-  ADMUX |= (1<<MUX2);
-  break;
-  case ADC_A5 :
-  ADMUX &=  ~((1<<MUX3) | (1<<MUX1));
-  ADMUX |= ((1<<MUX2) | (1<<MUX0));
-  break;
-  case ADC_A6 :
-  ADMUX &=  ~((1<<MUX3) | (1<<MUX0));
-  ADMUX |= ((1<<MUX2) | (1<<MUX1));
-  break;
-  case ADC_A7 :
-  ADMUX &=  ~((1<<MUX3) | (0<<MUX1));
-  ADMUX |= ((1<<MUX2)| (0<<MUX0));
-  break;
-  case ADC_SENSOR :
-  ADMUX &=  ~((1<<MUX2) | (1<<MUX1) | (1<<MUX0));
-  ADMUX |= ((1<<MUX3));
-  break;
-  case ADC_GND :
-  ADMUX |=  ~((0<<MUX3) | (0<<MUX2) | (0<<MUX1) | (0<<MUX0));
-  break;
-  case ADC_1V1 :
-  ADMUX |=  ~((0<<MUX3) | (0<<MUX2) | (0<<MUX1) );
-  ADMUX &= (0<<MUX0);
-  break;
+void setAnalogMux(ADC_modes mux)
+{
+    cli();
+    switch (mux)
+    {
+    case ADC_A0:
+        ADMUX &= ~((1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (1 << MUX0));
+
+        break;
+    case ADC_A1:
+        ADMUX &= ~((1 << MUX3) | (1 << MUX2) | (1 << MUX1));
+        ADMUX |= (1 << MUX0);
+        break;
+    case ADC_A2:
+        ADMUX &= ~((1 << MUX3) | (1 << MUX2) | (1 << MUX0));
+        ADMUX |= (1 << MUX1);
+        break;
+    case ADC_A3:
+        ADMUX &= ~((1 << MUX3) | (1 << MUX2));
+        ADMUX |= ((1 << MUX1) | (1 << MUX0));
+        break;
+    case ADC_A4:
+        ADMUX &= ~((1 << MUX3) | (1 << MUX1) | (1 << MUX0));
+        ADMUX |= (1 << MUX2);
+        break;
+    case ADC_A5:
+        ADMUX &= ~((1 << MUX3) | (1 << MUX1));
+        ADMUX |= ((1 << MUX2) | (1 << MUX0));
+        break;
+    case ADC_A6:
+        ADMUX &= ~((1 << MUX3) | (1 << MUX0));
+        ADMUX |= ((1 << MUX2) | (1 << MUX1));
+        break;
+    case ADC_A7:
+        ADMUX &= ~((1 << MUX3) | (0 << MUX1));
+        ADMUX |= ((1 << MUX2) | (0 << MUX0));
+        break;
+    case ADC_SENSOR:
+        ADMUX &= ~((1 << MUX2) | (1 << MUX1) | (1 << MUX0));
+        ADMUX |= ((1 << MUX3));
+        break;
+    case ADC_GND:
+        ADMUX |= ~((0 << MUX3) | (0 << MUX2) | (0 << MUX1) | (0 << MUX0));
+        break;
+    case ADC_1V1:
+        ADMUX |= ~((0 << MUX3) | (0 << MUX2) | (0 << MUX1));
+        ADMUX &= (0 << MUX0);
+        break;
+    }
+    sei();
 }
-sei();
-  
-}
-void ADC_setPrescaler(byte prescl){
-cli();
-  switch(prescl){
-    case 2 : 
-    ADCSRA &= ~((1<<ADPS2) | (1 << ADPS1) | (1<<ADPS0));
-    break;
-    case 4 : 
-    ADCSRA &= ~((1<<ADPS2)  | (1<<ADPS0));
-    ADCSRA |=  (1 << ADPS1);
-    break;
-    case 8 : 
-    ADCSRA &= ~(1<<ADPS2);
-    ADCSRA |=  ((1 << ADPS1) | (0<<ADPS0)) ;
-    
-    break;
-    case 16 : 
-    ADCSRA &= ~( (1 << ADPS1) | (1<<ADPS0));
-    ADCSRA |= (1<<ADPS2); 
-    break;
-    case 32 : 
-    ADCSRA &= ~((1 << ADPS1));
-    ADCSRA |= ((1<<ADPS2)| (1<<ADPS0));
-    break;
-    case 64 : 
-    ADCSRA &= ~((1<<ADPS0));
-    ADCSRA |= ((1<<ADPS2) | (1<<ADPS1));
-    break;
-    case 128 : 
-    ADCSRA |= ~((0<<ADPS2) | (0 << ADPS1) | (0<<ADPS0));
-    break;
-    
-  }
-  sei();
+void ADC_setPrescaler(byte prescl)
+{
+    cli();
+    switch (prescl)
+    {
+    case 2:
+        ADCSRA &= ~((1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0));
+        break;
+    case 4:
+        ADCSRA &= ~((1 << ADPS2) | (1 << ADPS0));
+        ADCSRA |= (1 << ADPS1);
+        break;
+    case 8:
+        ADCSRA &= ~(1 << ADPS2);
+        ADCSRA |= ((1 << ADPS1) | (0 << ADPS0));
+
+        break;
+    case 16:
+        ADCSRA &= ~((1 << ADPS1) | (1 << ADPS0));
+        ADCSRA |= (1 << ADPS2);
+        break;
+    case 32:
+        ADCSRA &= ~((1 << ADPS1));
+        ADCSRA |= ((1 << ADPS2) | (1 << ADPS0));
+        break;
+    case 64:
+        ADCSRA &= ~((1 << ADPS0));
+        ADCSRA |= ((1 << ADPS2) | (1 << ADPS1));
+        break;
+    case 128:
+        ADCSRA |= ~((0 << ADPS2) | (0 << ADPS1) | (0 << ADPS0));
+        break;
+    }
+    sei();
 }
 
 void ADC_setReference(ADC_modes ref)
 {
-  cli();
-  switch(ref){
-    case ADC_1V1 :
-    ADMUX |= (1<<REFS0);
-    ADMUX |=(1<<REFS1);
-    break;
-    case ADC_AREF :
-    ADMUX &= (0<<REFS0);
-    ADMUX &= (0<<REFS1);    
-    break;
-    case ADC_VCC :
-    ADMUX |= (1<<REFS0);
-    ADMUX &= (0<<REFS1); 
-    break;
-    
-  }
-  sei();
+    cli();
+    switch (ref)
+    {
+    case ADC_1V1:
+        ADMUX |= (1 << REFS0);
+        ADMUX |= (1 << REFS1);
+        break;
+    case ADC_AREF:
+        ADMUX &= (0 << REFS0);
+        ADMUX &= (0 << REFS1);
+        break;
+    case ADC_VCC:
+        ADMUX |= (1 << REFS0);
+        ADMUX &= (0 << REFS1);
+        break;
+    }
+    sei();
 }
-void leftAdjust(int ndr){
-switch(ndr)
+void leftAdjust(int ndr)
 {
-  case 8 :
-  ADMUX |= (1<<ADLAR);
-  break;
-  case 10 :
-  ADMUX &= (0 << ADLAR);
-  break; 
-  
-}
+    switch (ndr)
+    {
+    case 8:
+        ADMUX |= (1 << ADLAR);
+        break;
+    case 10:
+        ADMUX &= (0 << ADLAR);
+        break;
+    }
 }
 
 void (*adc_isr)();
 void (*acomp_isr)();
-void ADC_attachInterrupt(void (*isr)()) { 
+void ADC_attachInterrupt(void (*isr)())
+{
     cli();
     adc_isr = *isr;
-    ADCSRA |= (1 << ADIE); 
+    ADCSRA |= (1 << ADIE);
     sei();
 }
 
-void ADC_detachInterrupt() { 
+void ADC_detachInterrupt()
+{
     cli();
-    ADCSRA &= ~ (1 << ADIE); 
+    ADCSRA &= ~(1 << ADIE);
     sei();
 }
 
-void ADC_startConvert() { 
+void ADC_startConvert()
+{
     cli();
     ADCSRA |= (1 << ADSC);
     sei();
 }
 
-unsigned int ADC_read() { 
+unsigned int ADC_read()
+{
     return ADC;
 }
 
-uint8_t ADC_read8(void) { 
+uint8_t ADC_read8(void)
+{
     return ADCH;
 }
 
-boolean ADC_available() { 
-    if (ADCSRA & (1 << ADSC)) { // if flag set - convert in process
+boolean ADC_available()
+{
+    if (ADCSRA & (1 << ADSC))
+    { // if flag set - convert in process
         return false;
     }
-    else { // convert complete
+    else
+    { // convert complete
         return true;
     }
 }
 
-unsigned int ADC_readWhenAvailable() { 
-    while (ADCSRA & (1 << ADSC));
-    return ADC; 
+unsigned int ADC_readWhenAvailable()
+{
+    while (ADCSRA & (1 << ADSC))
+        ;
+    return ADC;
 }
 
-uint8_t ADC_read8WhenAvailable(void) {
-    while (ADCSRA & (1 << ADSC));
+uint8_t ADC_read8WhenAvailable(void)
+{
+    while (ADCSRA & (1 << ADSC))
+        ;
     return ADCH;
 }
 
-ISR(ADC_vect){ 
-    (*adc_isr)();
-}
+void setup()
+{
 
-void ACOMP_attachInterrupt(void (*isr)(), ADC_modes source) { 
-    cli();
-    acomp_isr = *isr;
-    ACSR |= (1 << ACIE); // set int bit
-    switch (source) {
-    case FALLING_TRIGGER: // falling
-        ACSR &= ~ (1 << ACIS0);
-        ACSR |= (1 << ACIS1);
-        break;
-    case RISING_TRIGGER: // rising
-        ACSR |=  ((1 << ACIS1) | (1 << ACIS0));
-        break;
-    case CHANGE_TRIGGER: // change (default)
-        ACSR &= ~ ((1 << ACIS1) | (1 << ACIS0));
-        break;
-    }
-    sei();
-}
+    byte x = 0;
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    display.clearDisplay();
+    display.fillRect(0, 0, display.width() - 2, 11, WHITE);
+    display.setTextColor(BLACK);
 
-void ACOMP_detachInterrupt() { 
-    cli();
-    ACSR &= ~ (1 << ACIE); 
-    sei();
-}
+    x = 16;
+    display.setCursor(x, 2);
+    display.print(F("AUDIO"));
+    x = 52;
+    display.setCursor(x, 2);
+    display.print(F("SPECTROMETER"));
 
-void ACOMP_enable() { 
-    cli();
-    ACSR &= ~ (1 << ACD); 
-    sei();
-}
-
-void ACOMP_disable() { 
-    cli();
-    ACSR |= (1 << ACD); // set bit "analog comp disable"
-    sei();
-}
-
-boolean ACOMP_read() { // 
-    return ACSR & (1 << ACO); 
-}
-
-void ACOMP_setPositiveInput(ADC_modes in) { 
-    cli();
-    switch (in) {
-    case ADC_1V1: // 1v1
-        ACSR |= (1 << ACBG); 
-        break;
-    case ADC_AIN0: // ADC_AIN0
-        ACSR &= ~ (1 << ACBG); // disconnect 1v1
-        break;
-    }
-    sei();
-}
-
-void ACOMP_setNegativeInput(ADC_modes in) {
-    cli();
-    switch (in) {
-    case ADC_AIN1: // ADC_AIN1 (default) - 
-        ADCSRB &= ~ (1 << ACME); // disable -in from mux
-        break;
-    case ANALOG_MUX: // analog MUX -
-        ADCSRB |= (1 << ACME); // connetct -in to analog mux
-        break;
-    }
-    sei();
-}
-
-
-
-
-ISR(ANALOG_COMP_vect){ 
-    (*acomp_isr)();
-}
-
-
-/* an example of a simple ace operation */
-void setup() {
-  Serial.begin(9600);
-  pinMode(led,OUTPUT);
-  pinMode(A0,INPUT);
-  ADC_enable(); 
-  ADC_setPrescaler(32);
-  //leftAdjust(8);
-  ADC_setReference(ADC_1V1); 
-  setAnalogMux(ADC_A0);
-}
-
-void loop() {
-    //digitalWrite(led, HIGH);
-  ADC_startConvert(); 
-  while (!ADC_available());
-  temp = ADC_read();
- //temp = analogRead(A0);
-  Serial.println(temp);
-    /*if(ADC_read()>= 1000)
+    for (byte i = 0; i < SAMPLES / 2 - 1; i++)
     {
-        digitalWrite(led, HIGH);  
-        delay(1000);  }
-        else 
-        digitalWrite(led,LOW);*/
+        display.drawFastHLine(i * 4 + 1, display.height() - 1, 3, WHITE);
+    }
+    display.setTextColor(WHITE);
+    display.display();
+    Serial.begin(9600);
+    pinMode(led, OUTPUT);
+    pinMode(A0, INPUT);
+    ADC_enable();
+    ADC_setPrescaler(32);
+    // leftAdjust(8);
+    ADC_setReference(ADC_1V1);
+    setAnalogMux(ADC_A0);
+}
 
+void loop()
+{
+
+    for (byte i = 0; i < SAMPLES; i++)
+    {
+
+        ADC_startConvert();
+        while (!ADC_available())
+        {
+            vReal[i] = ADC_read();
+            temp = ADC_read();
+            Serial.println(temp);
+        }
+        //////////////////////////////////
+        vImag[i] = 0;
+    }
+
+    FFT.Windowing(vReal, SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+    FFT.Compute(vReal, vImag, SAMPLES, FFT_FORWARD);
+    FFT.ComplexToMagnitude(vReal, vImag, SAMPLES);
+
+    display.fillRect(0, 12, display.width() - 2, display.height() - 13, BLACK);
+    for (byte i = 0; i < SAMPLES / 2 - 1; i++)
+    {
+        peak = map(vReal[i + 2], 0, 1024, 0, 52);
+        display.fillRect(i * 4 + 1, abs(52 - peak) + 12, 3, peak, WHITE);
+    }
+    maxpeak = FFT.MajorPeak(vReal, SAMPLES, 5000);
+
+    sprintf(buf, "%04li", maxpeak);
+    display.setCursor(72, 16);
+    display.print(F("Peak:"));
+    display.print(buf);
+    display.display();
+
+    /*
+     */
 }
--- Projet/src/TP2.cpp
+++ Projet/src/TP2.cpp
@@ -1,71 +1,9 @@
-/*#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 
-// defines for setting and clearing register bits
-// defining pins for control
-const int pinAnalogIn = A0;
 
-const int bufferSize = 128;
+void setup() {  
 
-const int minMode = 1;
-const int maxMode = 7 + 500;
-/
-const int defaultMode = 7;
-
-// initing display
-Adafruit_SSD1306 display(128, 32, &Wire, -1);
-byte buffer[bufferSize];
-
-int mode = defaultMode;
-
-void drawScreen(unsigned long duration) {
-  display.clearDisplay();
-  display.setTextColor(BLACK, WHITE);
-  for (int v = 5; v >= 0; v --) {
-    int tickY;
-    if (v == 5)
-      tickY = 0;
-    else if (! v)
-      tickY = display.height() - 1;
-    else
-      tickY = display.height() - display.height() * v / 5;
-    for (int x = 0; x < display.width(); x += 8)
-      display.drawPixel(x, tickY, WHITE);
-  }
-  
-  for (int x = 0; x < bufferSize; x ++) {
-    if (! x)
-      display.drawPixel(x, buffer[x], WHITE);
-    else
-      display.drawLine(x - 1, buffer[x - 1], x, buffer[x], WHITE);
-  }
-  
-  display.setCursor(0, 0);
-  display.print("5V");
-  display.setCursor(0, display.height() - 7);
-  display.print(duration);
-  display.print(" us");
-  display.display();
 }
 
-void setup() {
-  // defining pins for control
-  pinMode(pinAnalogIn, INPUT);
-  Serial.begin(115200);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C, false);
-  display.setTextSize(1);
-  display.clearDisplay();
-}
+void loop() {  
 
-void loop() {
-  
-  unsigned long delayUs = (mode > 7) ? (mode - 7) * 20 : 0;
-  unsigned long start = micros();
-  for (int x = 0; x < bufferSize; x ++) {
-    buffer[x] = map(analogRead(pinAnalogIn), 0, 1023, display.height() - 1, 0);
-    if (delayUs)
-      delayMicroseconds(delayUs);
-  }
-  drawScreen(micros() - start);
-}*/
+}
